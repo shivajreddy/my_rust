421d454c43ebdb96a3fd6ba022b28624d8b96efd
shivajreddy <shivajreddy@outlook.com>
2024-05-01 13:22:27 -0400

    .

--- rustling/src/boxy.rs
+++ rustling/src/boxy.rs
@@ -1,28 +1,55 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::mem;
+
 struct BigData {
+    // ppty1: i32,
+    // ppty2: i32,
+    // ppty6: i64,
+}
+
+struct SomeStruct {
     ppty1: i32,
     ppty2: i32,
     ppty3: i32,
-    ppty4: usize,
-    ppty5: i8,
-    ppty6: i64,
 }
 
-struct Node<T> {
-    val: T,
-    next: Option<Box<Node<T>>>,
+impl SomeStruct {
+    fn size(&self) {
+        let size = mem::size_of::<SomeStruct>();
+        println!("Size: {} bytes", size);
+    }
 }
 
-struct LinkedListNode<T> {
-    val: T,
-    prev: Option<Box<LinkedListNode<T>>>,
-    next: Option<Box<LinkedListNode<T>>>,
+impl BigData {
+    fn size(&self) {
+        let size = mem::size_of::<SomeStruct>();
+        println!("Size: {} bytes", size);
+    }
 }
 
 pub fn main() {
-    let x = Box::new(10);
+    let s1: SomeStruct = SomeStruct {
+        ppty1: 10,
+        ppty2: 20,
+        ppty3: 30,
+    };
+    s1.size();
 
-    println!("{} {0:p}", &x);
+    let b1: BigData = BigData {
+        // ppty1: 10,
+        // ppty2: 20,
+        // ppty6: 30,
+    };
+    b1.size();
+
+    let name = "hi".to_string();
+    println!("{}", mem::size_of_val(&name));
+
+    let x: i32 = 10;
+    println!("{} bytes", mem::size_of_val(&x));
+
+    let y: i64 = 10;
+    println!("{} bytes", mem::size_of_val(&y));
 }
