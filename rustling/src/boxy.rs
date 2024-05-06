c7b5222f4cfdd304c0a490f1463227e15182e4f3
shivajreddy <shivajreddy@outlook.com>
2024-05-06 15:42:00 -0400

    .

--- rustling/src/boxy.rs
+++ rustling/src/boxy.rs
@@ -3,53 +3,73 @@
 
 use std::mem;
 
+#[derive(Clone, Copy, Debug)]
 struct BigData {
-    // ppty1: i32,
-    // ppty2: i32,
-    // ppty6: i64,
-}
-
-struct SomeStruct {
     ppty1: i32,
     ppty2: i32,
-    ppty3: i32,
-}
-
-impl SomeStruct {
-    fn size(&self) {
-        let size = mem::size_of::<SomeStruct>();
-        println!("Size: {} bytes", size);
-    }
-}
-
-impl BigData {
-    fn size(&self) {
-        let size = mem::size_of::<SomeStruct>();
-        println!("Size: {} bytes", size);
-    }
+    ppty3: i64,
+    ppty4: i64,
+    ppty5: i32,
 }
 
 pub fn main() {
-    let s1: SomeStruct = SomeStruct {
+    let literal: BigData = BigData {
         ppty1: 10,
         ppty2: 20,
         ppty3: 30,
+        ppty4: 30,
+        ppty5: 20,
     };
-    s1.size();
+    println!("size_of_val(&b1) {} ", mem::size_of_val(&literal));
 
-    let b1: BigData = BigData {
-        // ppty1: 10,
-        // ppty2: 20,
-        // ppty6: 30,
-    };
-    b1.size();
+    let copy = literal.clone();
 
-    let name = "hi".to_string();
-    println!("{}", mem::size_of_val(&name));
+    println!("size_of_val(&copy) {}", mem::size_of_val(&copy));
 
-    let x: i32 = 10;
-    println!("{} bytes", mem::size_of_val(&x));
+    let box1 = Box::new(BigData {
+        ppty1: 10,
+        ppty2: 20,
+        ppty3: 30,
+        ppty4: 30,
+        ppty5: 20,
+    });
+    println!("box1 mem: {:p}", &box1);
+    println!("Size of box1: {}", mem::size_of_val(&box1));
+    println!("*box1 {:?}", *box1);
+    println!("*box1-heap-mem: {:p}", &(*box1));
 
-    let y: i64 = 10;
-    println!("{} bytes", mem::size_of_val(&y));
+    {
+        let box_copy = box1.clone();
+        // let box_copy = box1;
+        println!("box_copy mem: {:p}", &box_copy);
+        println!("Size of box_copy: {}", mem::size_of_val(&box_copy));
+        println!("*box_copy-heap-mem: {:p}", &(*box_copy));
+    }
+
+    println!("box1 mem: {:p}", &box1);
+    println!("Size of box1: {}", mem::size_of_val(&box1));
+
+    // println!("i32: {} bytes", mem::size_of::<i32>());
+    // println!("i64: {} bytes", mem::size_of::<i64>());
+    // println!("usize: {} bytes", mem::size_of::<usize>());
+}
+
+struct ListNode {
+    val: i32,
+    next: Option<Box<ListNode>>,
+}
+
+fn reverse_linked_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut prev: Option<Box<ListNode>> = None;
+    let mut curr = head;
+
+    while let Some(mut node) = curr {
+        curr = node.next.take();
+
+        node.next = prev;
+
+        prev = Some(node);
+    }
+
+    prev
 }
