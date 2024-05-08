6a95970163dd8494238226e062a4c44450743817
shivajreddy <shivajreddy@outlook.com>
2024-05-08 08:27:05 -0400

    exercism-helloword done

--- exercism/rust/hello-world/src/lib.rs
+++ exercism/rust/hello-world/src/lib.rs
@@ -1,4 +1,5 @@
 // &'static is a "lifetime specifier", something you'll learn more about later
 pub fn hello() -> &'static str {
-    "Goodbye, Mars!"
+    // "Goodbye, Mars!"
+    "Hello, World!"
 }
