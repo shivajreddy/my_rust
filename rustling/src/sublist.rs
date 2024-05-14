60715f0772c7437ea700da8aa9a0c1cfc7a66633
shivajreddy <shivajreddy@outlook.com>
2024-05-14 07:26:22 -0400

    .

--- rustling/src/sublist.rs
+++ rustling/src/sublist.rs
@@ -8,32 +8,32 @@ If A = [1, 2, 3, 4, 5] and B = [2, 3, 4], then A is a superlist of B
 */
 
 pub fn main() {
-    let a = [3, 4];
-    let b = [1, 2, 3, 4, 5];
-    // let b = [];
-    // result is yes
+    let a = [1, 2, 3, 4, 5];
+    let b = [3, 4];
+
+    let a = [1, 2, 3, 3, 3, 4, 5];
+    let b = [3, 4];
+
     let result: bool;
 
     // check if the same sequence of numbers exist
-    {
-        let mut possible_starting_indices: Vec<usize> = vec![];
-        let target_starting_number: i32 = b[0];
-        // get all locations of the starting number
-        for (idx, num) in a.iter().enumerate() {
-            if *num == target_starting_number {
-                possible_starting_indices.push(idx);
-            }
-            println!("{} {}", idx, num);
+    let mut possible_starting_indices: Vec<usize> = vec![];
+    let target_starting_number: i32 = b[0];
+    // get all locations of the starting number
+    for (idx, num) in a.iter().enumerate() {
+        if *num == target_starting_number {
+            possible_starting_indices.push(idx);
         }
-        println!("possible_starting_indices:{:?}", possible_starting_indices);
+        println!("{} {}", idx, num);
     }
+    println!("possible_starting_indices:{:?}", possible_starting_indices);
 
-    let a = [3, 4];
-    let b = [1, 2, 3, 3, 3, 4, 5];
+    a.
 
     let a = [1, 2, 3];
     let b = [1, 2, 3];
 
     let a = [1, 2, 3, 4, 5];
     let b = [2, 3, 4];
+
 }
