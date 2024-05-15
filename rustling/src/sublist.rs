50241d01b72007b7265874955349ee01a36f290f
shivajreddy <shivajreddy@outlook.com>
2024-05-15 10:00:51 -0400

    .

--- rustling/src/sublist.rs
+++ rustling/src/sublist.rs
@@ -2,28 +2,6 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
-/*
-If A = [3, 4] and B = [1, 2, 3, 4, 5], then A is a sublist of B
-If A = [1, 2, 3] and B = [1, 2, 3], then A and B are equal
-If A = [1, 2, 3, 4, 5] and B = [2, 3, 4], then A is a superlist of B
-*/
-
-// pub fn main() {
-//     let a = [1, 2, 3, 4, 5];
-//     let b = [3, 4];
-//
-//     let a = [1, 2, 3, 3, 3, 4, 5];
-//     let b = [3, 4];
-//
-//     let a = [1, 2, 3];
-//     let b = [1, 2, 3];
-//
-//     let a = [1, 2, 3, 4, 5];
-//     let b = [2, 3, 4];
-// }
-
-use std::any::type_name_of_val;
-
 pub fn main() {
     let arr = [10, 20, 30];
 }
