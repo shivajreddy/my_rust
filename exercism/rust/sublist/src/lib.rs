b91ab67603309c0ef0911c0b5b8f04a8eddeacc5
shivajreddy <shivajreddy@outlook.com>
2024-05-15 13:48:26 -0400

    Finished SubList

--- exercism/rust/sublist/src/lib.rs
+++ exercism/rust/sublist/src/lib.rs
@@ -17,19 +17,17 @@ fn is_super_list<T: PartialEq>(first_list: &[T], second_list: &[T]) -> bool {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    // todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
-
     // Equal check: both lists are entirely equal
     if _first_list == _second_list {
         return Comparison::Equal;
     }
 
-    // Sublist check:
+    // Sublist check
     if is_super_list(_first_list, _second_list) {
         return Comparison::Superlist;
     }
 
-    // Superlist check:
+    // Superlist check
     if is_super_list(_second_list, _first_list) {
         return Comparison::Sublist;
     }
