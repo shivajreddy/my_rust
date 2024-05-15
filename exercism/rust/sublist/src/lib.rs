2b50eb65b615783081186aa6ed087dad6fa48e3d
shivajreddy <shivajreddy@outlook.com>
2024-05-15 10:00:30 -0400

    working on sublist

--- exercism/rust/sublist/src/lib.rs
+++ exercism/rust/sublist/src/lib.rs
@@ -7,5 +7,18 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    // todo!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+
+    // Equal check: both lists are entirely equal
+    if _first_list == _second_list {
+        return Comparison::Equal;
+    }
+
+    // Sublist check:
+
+    // Superlist check:
+
+    // Unequal check: if its not any of the above,
+    // then it has to be unequal
+    Comparison::Unequal
 }
--- rustling/src/sublist.rs
+++ rustling/src/sublist.rs
@@ -1,5 +1,6 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused_imports)]
 
 /*
 If A = [3, 4] and B = [1, 2, 3, 4, 5], then A is a sublist of B
@@ -7,39 +8,22 @@ If A = [1, 2, 3] and B = [1, 2, 3], then A and B are equal
 If A = [1, 2, 3, 4, 5] and B = [2, 3, 4], then A is a superlist of B
 */
 
+// pub fn main() {
+//     let a = [1, 2, 3, 4, 5];
+//     let b = [3, 4];
+//
+//     let a = [1, 2, 3, 3, 3, 4, 5];
+//     let b = [3, 4];
+//
+//     let a = [1, 2, 3];
+//     let b = [1, 2, 3];
+//
+//     let a = [1, 2, 3, 4, 5];
+//     let b = [2, 3, 4];
+// }
+
+use std::any::type_name_of_val;
+
 pub fn main() {
-    let a = [1, 2, 3, 4, 5];
-    let b = [3, 4];
-
-    let a = [1, 2, 3, 3, 3, 4, 5];
-    let b = [3, 4];
-
-    let result: bool;
-
-
-    // Equal check: both lists are entirely equal
-    if a == b {
-        return 
-    }
-
-    // check if the same sequence of numbers exist
-    let mut possible_starting_indices: Vec<usize> = vec![];
-    let target_starting_number: i32 = b[0];
-    // get all locations of the starting number
-    for (idx, num) in a.iter().enumerate() {
-        if *num == target_starting_number {
-            possible_starting_indices.push(idx);
-        }
-        println!("{} {}", idx, num);
-    }
-    println!("possible_starting_indices:{:?}", possible_starting_indices);
-
-    a.
-
-    let a = [1, 2, 3];
-    let b = [1, 2, 3];
-
-    let a = [1, 2, 3, 4, 5];
-    let b = [2, 3, 4];
-
+    let arr = [10, 20, 30];
 }
