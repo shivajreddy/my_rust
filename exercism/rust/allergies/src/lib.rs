f4798f3880ec3c3dd2241be6308f45bc76dda1de
shivajreddy <shivajreddy@outlook.com>
2024-05-20 17:40:24 -0400

    Allergies Finished

--- exercism/rust/allergies/src/lib.rs
+++ exercism/rust/allergies/src/lib.rs
@@ -1,6 +1,4 @@
-pub struct Allergies;
-
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum Allergen {
     Eggs,
     Peanuts,
@@ -12,16 +10,61 @@ pub enum Allergen {
     Cats,
 }
 
+impl Allergen {
+    pub fn value(&self) -> u32 {
+        match self {
+            Allergen::Eggs => 1,
+            Allergen::Peanuts => 2,
+            Allergen::Shellfish => 4,
+            Allergen::Strawberries => 8,
+            Allergen::Tomatoes => 16,
+            Allergen::Chocolate => 32,
+            Allergen::Pollen => 64,
+            Allergen::Cats => 128,
+        }
+    }
+}
+
+const ALL_ALLERGENS: &[Allergen] = &[
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
+
+pub struct Allergies {
+    score: u32,
+}
+
 impl Allergies {
     pub fn new(score: u32) -> Self {
-        todo!("Given the '{score}' score, construct a new Allergies struct.");
+        // make sure the score is in the range 1, 255 (inclusive)
+        let mut given_score = score;
+        while given_score >= 256 {
+            given_score -= 256
+        }
+        Allergies { score: given_score }
     }
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
-        todo!("Determine if the patient is allergic to the '{allergen:?}' allergen.");
+        self.allergies().contains(&allergen)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        todo!("Return the list of allergens contained within the score with which the Allergies struct was made.");
+        let mut result: Vec<Allergen> = vec![];
+        let mut total_score = self.score;
+
+        for allergen in ALL_ALLERGENS.iter().rev() {
+            if total_score >= allergen.value() {
+                total_score -= allergen.value();
+                result.push(*allergen);
+            }
+        }
+
+        result
     }
 }
--- rustling/src/allergies.rs
+++ rustling/src/allergies.rs
@@ -1,3 +1,67 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+
+use std::{borrow::Borrow, thread::current};
+
+#[derive(Debug, Clone, Copy)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+impl Allergen {
+    // hey man, use u8 instead
+    pub fn value(&self) -> u32 {
+        match self {
+            Allergen::Eggs => 1,
+            Allergen::Peanuts => 2,
+            Allergen::Shellfish => 4,
+            Allergen::Strawberries => 8,
+            Allergen::Tomatoes => 16,
+            Allergen::Chocolate => 32,
+            Allergen::Pollen => 64,
+            Allergen::Cats => 128,
+        }
+    }
+}
+
+const ALL_ALLERGENS: &[Allergen] = &[
+    Allergen::Eggs,
+    Allergen::Peanuts,
+    Allergen::Shellfish,
+    Allergen::Strawberries,
+    Allergen::Tomatoes,
+    Allergen::Chocolate,
+    Allergen::Pollen,
+    Allergen::Cats,
+];
+
 pub fn main() {
-    println!("allergies");
+    // let mut result: Vec<Allergen> = vec![];
+    //
+    // let mut given_value = 80;
+    // for allergen in ALL_ALLERGENS.iter().rev() {
+    //     // println!(" {:?} {}", allergen, allergen.value());
+    //
+    //     if allergen.value() <= given_value {
+    //         given_value -= allergen.value();
+    //         result.push(*allergen);
+    //     }
+    // }
+    //
+    // println!("{:?}", result);
+    let mut given_score = 509;
+    let mut given_score = 257;
+    while given_score > 256 {
+        given_score -= 256
+    }
+    println!("{}", given_score);
 }
