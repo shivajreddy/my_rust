c2d12dd61bd2055aa2d7c67c6bfe578b8df86a8f
shivajreddy <shivajreddy@outlook.com>
2024-05-20 18:24:04 -0400

    Gigasecond - Finished

--- exercism/rust/gigasecond/src/lib.rs
+++ exercism/rust/gigasecond/src/lib.rs
@@ -1,6 +1,11 @@
+use std::{ops::Add, time::Duration};
+
 use time::PrimitiveDateTime as DateTime;
+
+const GIGA: u64 = 1000_000_000; // Giga = thousand, million
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    todo!("What time is a gigasecond later than {start}");
+    let giga_seconds = Duration::new(GIGA, 0);
+    start.add(giga_seconds)
 }
