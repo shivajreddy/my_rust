829f71ea9e397632d455670a38937fdfa0be40a1
shivajreddy <shivajreddy@outlook.com>
2024-05-20 20:16:42 -0400

    Luhn - working

--- exercism/rust/luhn/src/lib.rs
+++ exercism/rust/luhn/src/lib.rs
@@ -1,4 +1,41 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    todo!("Is the Luhn checksum for {code} valid?");
+    let all_digits = get_all_digits(code);
+    // println!("all-digits: {:?}", all_digits);
+
+    let mut doubled_digits: Vec<u8> = vec![];
+
+    for (idx, digit) in all_digits.iter().enumerate() {
+        if idx % 2 == 0 {
+            // println!("{}:{} idx%2={}", idx, digit, idx % 2);
+            let doubled = if digit * 2 > 9 {
+                (digit * 2) - 9
+            } else {
+                digit * 2
+            };
+            doubled_digits.push(doubled);
+        } else {
+            doubled_digits.push(*digit);
+        }
+    }
+    // println!("doubled={:?}", doubled_digits);
+
+    let mut sum_digits = 0;
+    for digit in &doubled_digits {
+        sum_digits += digit;
+    }
+    // println!("sum_digits={}", sum_digits);
+
+    let result = sum_digits % 10 == 0;
+    // println!("{}", result);
+    result
+}
+
+fn get_all_digits(code: &str) -> Vec<u8> {
+    let mut result = vec![];
+    for c in code.chars() {
+        let num = c.to_digit(10).unwrap() as u8;
+        result.push(num);
+    }
+    result
 }
--- rustling/src/allergies.rs
+++ rustling/src/allergies.rs
@@ -4,35 +4,79 @@
 #![allow(unused_mut)]
 
 pub fn main() {
-    let num: u32 = 4_106_098_957;
+    // let num = "1789372997";
+    // let num = "4539319503436467";
+    // let num = "8273123273520569";
+    let num = "0";
 
+    let all_digits = get_all_digits(num);
+    println!("all-digits: {:?}", all_digits);
+
+    let mut doubled_digits: Vec<u8> = vec![];
+
+    for (idx, digit) in all_digits.iter().enumerate() {
+        if idx % 2 == 0 {
+            // println!("{}:{} idx%2={}", idx, digit, idx % 2);
+            let doubled = if digit * 2 > 9 {
+                (digit * 2) - 9
+            } else {
+                digit * 2
+            };
+            doubled_digits.push(doubled);
+        } else {
+            doubled_digits.push(*digit);
+        }
+    }
+    println!("doubled={:?}", doubled_digits);
+
+    let mut sum_digits = 0;
+    for digit in &doubled_digits {
+        sum_digits += digit;
+    }
+    println!("sum_digits={}", sum_digits);
+
+    let result = sum_digits % 10 == 0;
+    println!("{}", result);
+
+    // println!("sum_digits={}", sum_digits);
+
+    // let formula_result = 10 - (sum_digits % 10);
+    // println!("formula_result={}", formula_result);
+}
+
+fn get_all_digits(code: &str) -> Vec<u8> {
+    let mut result = vec![];
+    for c in code.chars() {
+        let num = c.to_digit(10).unwrap() as u8;
+        result.push(num);
+        // println!("{}", num);
+    }
+    result
+}
+
+fn get_all_digits_of_number(num: u32) -> Vec<u8> {
+    let mut result = vec![];
     let mut n = num;
-
-    // get the list of digits
-    let mut all_digits: Vec<u32> = vec![];
-
     if n == 0 {
-        all_digits.push(0)
-    }
-
+        result.push(0)
+    };
     while n > 0 {
-        let ones_place = n % 10;
-        n = n / 10;
-        all_digits.push(ones_place);
-
-        // println!("{}", ones_place);
+        let digit = n % 10;
+        n /= 10;
+        result.push(digit as u8);
     }
-    println!("all_digits: {:?}", all_digits);
+    result
+}
 
-    let no_of_digits = all_digits.len();
+fn sum_digits_in_number(num: u32) -> u32 {
+    let mut result: u32 = 0;
+    let all_digits = get_all_digits_of_number(num);
 
-    let mut new_number: u32 = 0;
+    // println!("{} {:?}", num, all_digits);
 
     for digit in all_digits {
-        new_number += digit.pow(no_of_digits as u32);
+        result += digit as u32;
     }
-    println!("old={} new={}", num, new_number);
 
-    let result = num == new_number;
-    // println!("{}", result);
+    result
 }
