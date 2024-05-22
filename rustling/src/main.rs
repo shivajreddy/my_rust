392c0b0e3ff8f9ddb08aaac7274f67427d333cf0
shivajreddy <shivajreddy@outlook.com>
2024-05-22 06:45:59 -0400

    moved files

--- rustling/src/main.rs
+++ rustling/src/main.rs
@@ -3,12 +3,14 @@
 // mod playground;
 // mod some;
 // mod minesweeper;
-mod allergies;
+// mod allergies;
+mod luhn;
 
 fn main() {
     // boxy::main();
     // lru::main();
     // playground::main();
     // some::main();
-    allergies::main();
+    // allergies::main();
+    luhn::main();
 }
