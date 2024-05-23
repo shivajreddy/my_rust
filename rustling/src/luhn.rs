d7762016d0825d88ecd039215692423972cfef6f
shivajreddy <shivajreddy@outlook.com>
2024-05-23 17:51:18 -0400

    .

--- rustling/src/luhn.rs
+++ rustling/src/luhn.rs
@@ -1,3 +1,29 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+
 pub fn main() {
     println!("luhn");
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+// Create a new test module
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 3), 5);
+    }
+
+    #[test]
+    fn test_add2() {
+        assert_eq!(add(2, -3), 1);
+    }
 }
