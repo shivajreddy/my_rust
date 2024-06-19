d0bf741a500c164d1f2ce0b294f2e8c8a6ceb0df
shivajreddy_wsl <shivajreddy@outlook.com>
2024-06-19 15:07:55 -0400

    .

--- tui/hello-ratatui/src/main.rs
+++ tui/hello-ratatui/src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 // crossterm necessary to run the application
 use crossterm::{
     event::{self, KeyCode, KeyEventKind},
-    terminal::{disable_raw_mode, enable_raw_mode, 
+    terminal::{disable_raw_mode, enable_raw_mode,
                EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand
 };
@@ -20,8 +20,8 @@ use ratatui::{
 use std::io::{stdout, Result};
 
 
-/// Add code to set up and restore the terminal 
-/// 1. App enters the 'alternate screen' 
+/// Add code to set up and restore the terminal
+/// 1. App enters the 'alternate screen'
 /// - allows app to render, without disturbing normal term.
 /// 2. App enables the 'raw mode'
 /// - this turns off input and output process by terminal
@@ -33,20 +33,23 @@ use std::io::{stdout, Result};
 fn main() -> Result<()> {
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
-    
+
     Ok(())
 }
 // */
 
-
-use std::fmt::{Debug, Display, Formatter};
+use std::fmt::{Debug, Formatter};
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, stdout, Read, Result};
 use std::mem::size_of_val;
 
-// fn main() -> Result<String, io::Error> { 
-fn main() {
+// fn main() -> Result<String, io::Error> {
+fn main() -> Result<()> {
     // create a file with text
+    let file = File::create("foo.txt");
+    println!("{}", size_of_val(&file));
+    Ok(())
+    //Result::Ok("done".to_string())
     // let mut new_file = File::create("bar.txt");
 
     // create a file in target directory
@@ -58,52 +61,10 @@ fn main() {
     // copy data from one file to another
 }
 
-
-enum Shape {
-    Circle(f64),
-    Rectangle(f64, f64),
-}
-
-impl Shape {
-    // Method to calculate area of the shape
-    fn area(&self) -> f64 {
-        // Encapsulating the logic specific to each variant
-        match self {
-            Shape::Circle(radius) => {
-                std::f64::consts::PI * radius * radius
-            }
-            Shape::Rectangle(length, width) => {
-                length * width
-            }
-        }
-    }
-    // Method to calculate the perimeter of a shape
-    fn perimeter(&self) -> f64 {
-        match self {
-            Shape::Circle(radius) => 2.0 * std::f64::consts::PI * radius,
-            Shape::Rectangle(width, height) => 2.0 * (width + height),
-        }
+fn something(x: i32) -> Result<i32, String> {
+    if x > 0 {
+        Result::Ok(x)
+    } else {
+        Result::Err("wrong".to_string())
     }
 }
-
-
-use std::fmt;
-
-impl Display for Shape {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self {
-            Shape::Circle(r) => write!(f, "<Circle r:{r}>"),
-            Shape::Rectangle(l, w) => write!(f, "<Rectangle l:{l} w:{w}>")
-        }
-    }
-}
-
-
-enum Result<T, E>
-    where E: Display
-{
-    Ok(T),
-    Err(E),
-}
-
-
