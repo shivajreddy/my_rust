f1ba16896ce19756c2ee84402d7fbbeff24ee90f
shivajreddy_wsl <shivajreddy@outlook.com>
2024-06-24 09:53:24 -0400

    trying windows gui library

--- tui/hello-ratatui/src/main.rs
+++ tui/hello-ratatui/src/main.rs
@@ -40,19 +40,50 @@ fn main() -> Result<()> {
 
 use std::fmt::{Debug, Formatter};
 use std::fs::File;
-use std::io::{self, stdout, Read, Result, Write};
+use std::io::{self, stdout, Error, Read, Result, Write};
 use std::mem::size_of_val;
+use std::ops::Deref;
+
+#[cfg(target_os = "windows")]
+fn os_specific_function() {
+    pritnln!("This is windows specific");
+}
+
+#[cfg(target_os = "macos")]
+fn os_specific_function() {
+    pritnln!("This is MacOs specific");
+}
 
 // fn main() -> Result<String, io::Error> {
 fn main() -> Result<()> {
-    // create a file with text
-    let mut file = File::create("foo.txt")?;
-    file.write_all(b"wow\n")?;
-    file.write_all(b"wow")?;
+    let result_file = std::fs::File::open("foo.txt");
+    let mut file = result_file.unwrap(); // panics if file isn't found
+                                         // create a file with text
+                                         // let mut file = File::create("foo.txt")?;
+                                         // file.write_all(b"wow\n")?;
+                                         // file.write_all(b"wow")?;
 
-    println!("done");
+    let mut buffer = vec![];
 
-    Ok(())
+    // check the current operating system
+    let x = file.read_to_end(&mut buffer);
+
+    let r1 = cfg!(target_os = "windows");
+    let r2 = cfg!(target_os = "macos");
+    let r3 = cfg!(target_os = "linux");
+    println!("{r1}{r2}{r3}");
+
+    if cfg!(target_os = "windows") {
+        println!("you are running a windows")
+    };
+
+    println!("{:?}", buffer);
+
+    // let mut my_boxed: Box<str> = long_string.into_boxed_str();
+    // println!("{my_boxed}");
+
+    // long_string.push_str("new bytes");
+    // println!("{long_string}");
     // let mut new_file = File::create("bar.txt");
 
     // create a file in target directory
@@ -62,4 +93,14 @@ fn main() -> Result<()> {
     // read a file, and print content to console, line by line
 
     // copy data from one file to another
+
+    std::io::Result::Ok(())
+}
+
+struct SomeType {}
+
+impl SomeType {
+    fn something() {
+        println!("something is executed");
+    }
 }
